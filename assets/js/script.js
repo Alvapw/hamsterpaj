85d128dd1c6a6a63ce581c78c73edbc3ddbb88d9
Alva Petersson West <alva.p.west@gmail.com>
2024-04-24 18:30:26 +0200

    uppdaterat variabler, .c-post, js-menu

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+console.log('hej');
+// TO-DO: separat fil?
+// const menuIcon = document.querySelector('[data-fn-toggle-mobile-menu]');
+// const menuList = document.querySelector('[data-nav-list]');
+
+// // om menuicon finns så sätter vi på en lyssnare
+// if (menuIcon) {
+//     menuIcon.addEventListener('click', function () {
+//         // på click så öppnas nav list och icon blir till kryss
+//         menuIcon.classList.toggle('exit');
+//     });
+// }
