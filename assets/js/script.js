a8e301db7a3eafb767f30598f503ddc6bca5f7b0
Alva Petersson West <alva.p.west@gmail.com>
2024-04-26 09:01:11 +0200

    komponenter, filter, fler sidor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,7 @@
-console.log('hej');
-// TO-DO: separat fil?
-// const menuIcon = document.querySelector('[data-fn-toggle-mobile-menu]');
-// const menuList = document.querySelector('[data-nav-list]');
+const menuIcon = document.querySelector("[data-fn-toggle-menu]");
+const menuList = document.querySelector("[data-nav-list]");
 
-// // om menuicon finns så sätter vi på en lyssnare
-// if (menuIcon) {
-//     menuIcon.addEventListener('click', function () {
-//         // på click så öppnas nav list och icon blir till kryss
-//         menuIcon.classList.toggle('exit');
-//     });
-// }
+menuIcon.addEventListener("click", function () {
+  menuList.classList.toggle("active");
+  menuIcon.classList.toggle("exit");
+});
