9e5af1daae7219a6663f20ae41074cdf63aa399b
Alva Petersson West <alva.p.west@gmail.com>
2024-05-06 08:37:08 +0200

    footer + cards

--- assets/js/forum.js
+++ assets/js/forum.js
@@ -10,24 +10,30 @@ const forumFeed = document.querySelector("[data-forum-feed]");
 const searchInput = document.querySelector("[data-search]");
 const tags = document.querySelectorAll("[data-tag]");
 
+// lyssnar efter input i sökfältet och hämtar värdet
 searchInput.addEventListener("input", (e) => {
   const valueInput = e.target.value.toLowerCase();
   filters.search = valueInput;
   render();
 });
+
+// hämtar json data och pushar in det i arrayen forumThreads
 fetch("assets/json/forumData.json")
   .then((response) => response.json())
   .then((data) => {
     data.forEach((thread) => {
-      //forumThreads.push({ filtered: false, domNode: null, ...recept });
       forumThreads.push(thread);
     });
     render();
   })
   .catch((error) => console.log(error));
+
+// funktion för att skapa korten
 function createPostTeaser(thread) {
+  // klonar template
   const post = postTeaserTemplate.content.cloneNode(true).children[0];
 
+  // byter ut datan i kortet
   const createdDate = post.querySelector("[data-replace-created-date]");
   createdDate.textContent = thread.dateStarted;
 
@@ -49,19 +55,21 @@ function createPostTeaser(thread) {
   const comments = post.querySelector("[data-replace-comments]");
   comments.textContent = thread.comments;
 
+  // filtrerar ut hur många och vilka tags som ska skrivas ut i kortet och skapar elementet med rätt class
   const tagsContainer = post.querySelector("[data-replace-tags]");
   thread.tags.forEach((tag) => {
     console.log(tag);
     const spanTag = document.createElement("span");
-    spanTag.classList.add("c-badge");
+    spanTag.classList.add("c-tag");
     spanTag.textContent = tag;
     tagsContainer.append(spanTag);
   });
 
-  //   link.setAttribute("href", recept.html);
-  //   header.textContent = recept.name;
+  // spottar ut kortet i rätt container
   forumFeed.append(post);
 }
+
+// funktion för att rendera ut korten
 function render() {
   let filteredThreads = [...forumThreads];
   if (filters.search.length > 0) {
@@ -78,6 +86,7 @@ function render() {
   forumFeed.innerHTML = "";
   filteredThreads.forEach((thread) => createPostTeaser(thread));
 }
+
 console.log(tags);
 tags.forEach((tag) => {
   console.log(tag);
@@ -88,6 +97,53 @@ tags.forEach((tag) => {
     render();
   });
 });
+
+// hämtar alla dropdowns
+const dropdowns = document.querySelectorAll("[data-fn-dropdown]");
+
+// loopar igenom alla dropdowns
+dropdowns.forEach((dropdown) => {
+  // hämtar alla inre element i dropdowns
+  const select = dropdown.querySelector("[data-dropdown-select]");
+  const selected = dropdown.querySelector("[data-dropdown-selected]");
+  const caret = dropdown.querySelector("[data-dropdown-caret]");
+  const list = dropdown.querySelector("[data-dropdown-list]");
+  const options = dropdown.querySelectorAll("[data-dropdown-option]");
+
+  // lägger till en lyssnare på select
+  select.addEventListener("click", () => {
+    // togglar active class på select
+    select.classList.toggle("open");
+    // togglar rotate class på caret
+    caret.classList.toggle("rotate");
+    // togglar open class på list
+    list.classList.toggle("open");
+  });
+
+  // loopar igenom alla options
+  options.forEach((option) => {
+    // lägger till lyssnare på option
+    option.addEventListener("click", () => {
+      console.log("click");
+      // när ett option klickas på så ska selected innerText ändras till det klickade optionets innerText
+      selected.innerText = option.innerText;
+      // tar bort classer som förut lades till
+      select.classList.remove("open");
+      caret.classList.remove("rotate");
+      list.classList.remove("open");
+
+      // loopar igenom alla options för att se om något option redan har classen active
+      options.forEach((option) => {
+        if (option.classList.contains("active")) {
+          option.classList.remove("active");
+        }
+      });
+      // sen kan vi lägga till active class på det klickade optionet
+      option.classList.toggle("active");
+    });
+  });
+});
+
 // const select = document.querySelector("#forum-sort");
 // select.addEventListener("change", (e) => {
 //   // const currentTag = e.options;
